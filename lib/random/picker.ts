9f294676956f4f17127ad140d1234e0dd909b96b
Nkowne63 <neutron63zf@gmail.com>
2022-01-25 01:16:21 +0900

    fix spell

--- lib/random/picker.ts
+++ lib/random/picker.ts
@@ -1,4 +1,4 @@
-export const pickString = (words: string[], count: number) =>
+export const pickStrings = (words: string[], count: number) =>
 	Array.from(
 		{ length: count },
 		() => words[Math.floor(Math.random() * words.length)]
