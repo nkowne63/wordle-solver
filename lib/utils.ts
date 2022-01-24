6c9b52055293253c5e910930c3fef86d8c919ceb
Nkowne63 <neutron63zf@gmail.com>
2022-01-25 01:16:32 +0900

    fix: util

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,10 @@
-import { CharactersType } from "./information/enums";
-import { Status } from "./information/enums";
-import { wordFilters } from "./information/filters";
+import {
+	CharactersType,
+	FiveStringStatusType,
+	Status,
+} from "./information/enums";
+import { statusToConditions } from "./information/informations";
+
 const SimpleStatus = {
 	green: "g",
 	yellow: "y",
@@ -12,36 +16,24 @@ export const statusFilters = (
 	word: string,
 	status: string
 ) => {
-	const conditions = status.split("").map((s, position) => {
-		if (word.split("").indexOf(word[position]) !== position) {
-			// 2回目以降の出現は注意する必要がある
+	const serializedStatus = status
+		.split("")
+		.map((s) => {
 			switch (s) {
+				case SimpleStatus.green:
+					return Status.Correct;
 				case SimpleStatus.yellow:
-					return (target: string) =>
-						target.split("").filter((c) => c === word[position]).length > 1;
+					return Status.WrongPosition;
 				case SimpleStatus.grey:
-					return (target: string) =>
-						target.split("").filter((c) => c === word[position]).length === 1;
+					return Status.NotIn;
 				default:
-					return () => true;
+					throw new Error("invalid status");
 			}
-		}
-		switch (s) {
-			case SimpleStatus.green:
-				return wordFilters[Status.Correct](
-					word[position] as CharactersType,
-					position
-				);
-			case SimpleStatus.yellow:
-				return wordFilters[Status.WrongPosition](
-					word[position] as CharactersType,
-					position
-				);
-			case SimpleStatus.grey:
-				return wordFilters[Status.NotIn](word[position] as CharactersType);
-			default:
-				throw new Error("Unknown status");
-		}
-	});
+		})
+		.map((s, position) => ({
+			char: word[position] as CharactersType,
+			status: s,
+		})) as FiveStringStatusType;
+	const conditions = statusToConditions(serializedStatus);
 	return conditions.reduce((list, current) => list.filter(current), words);
 };
