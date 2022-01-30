270ce5a0615a5756a8f831515841edf4c74d2220
Nkowne63 <neutron63zf@gmail.com>
2022-01-30 14:04:44 +0900

    rename: util -> repl-util

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 export { next } from "./lib/information-maximum";
 export { words } from "./lib/words";
-export { statusFilters as filter } from "./lib/utils";
+export { statusFilters as filter } from "./lib/repl-util";
 
 console.log(`
 var list = w.words;
