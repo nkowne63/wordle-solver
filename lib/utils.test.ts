51b65ccf9249150f6d0f4a8af62986690b787aa5
Nkowne63 <neutron63zf@gmail.com>
2022-01-30 14:14:07 +0900

    add: structuredEq

--- lib/utils.test.ts
+++ lib/utils.test.ts
@@ -1,9 +1,28 @@
-import { assertsFive } from "./utils";
+import { assertsFive, structuredEqual } from "./utils";
 
 describe("utils", () => {
 	it("assetsFive", () => {
 		const n = [1, 2, 3, 4, 5];
 		assertsFive(n);
 		expect(n).toBe(n);
+	});
+	it("structuredEqual: equal", () => {
+		expect(structuredEqual("a", "a")).toBeTruthy();
+		expect(structuredEqual(0, 0)).toBeTruthy();
+	});
+	it("structuredEqual: not equal", () => {
+		expect(structuredEqual("a", "b")).toBeFalsy();
+		expect(structuredEqual(0, 1)).toBeFalsy();
+	});
+	it("structuredEqual: array", () => {
+		expect(structuredEqual(["a"], ["a"])).toBeTruthy();
+	});
+	it("structuredEqual: object", () => {
+		expect(structuredEqual({ a: "a" }, { a: "a" })).toBeTruthy();
+	});
+	it("structuredEqual: deep array", () => {
+		const a = [{ a: { b: [1, 2, 3] } }, "a"];
+		const b = [{ a: { b: [1, 2, 3] } }, "a"];
+		expect(structuredEqual(a, b)).toBeTruthy();
 	});
 });
--- lib/utils.ts
+++ lib/utils.ts
@@ -5,3 +5,14 @@ export const assertsFive: <T>(a: T[]) => asserts a is [T, T, T, T, T] = (
 	if (array.length !== 5)
 		throw new Error("assertsFive: Array must have 5 elements");
 };
+
+export const structuredEqual = <T>(a: T, b: T): boolean => {
+	if (a === b) {
+		return true;
+	} else if (Array.isArray(a) && Array.isArray(b)) {
+		return a.length === b.length && a.every((v, i) => structuredEqual(v, b[i]));
+	} else if (typeof a === "object" && typeof b === "object") {
+		return structuredEqual(Object.entries(a), Object.entries(b));
+	}
+	return false;
+};
